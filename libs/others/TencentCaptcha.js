1aa72bd1a1dbad25ca393034012bb733e008d436
MUG-chen <hanming.chen@outlook.com>
2024-02-02 19:44:13 +0800

    Site updated: 2024-02-02 19:44:09

---
+++ libs/others/TencentCaptcha.js
@@ -0,0 +1,1 @@
+onIsOff=!0,window.callback=function(t){0===t.ret&&window.clearInterval(t2),2===t.ret&&(onIsOff=!0)};var t2=window.setInterval((function(){1==onIsOff&&(act=document.activeElement.id,"veditor"==act&&(document.getElementById("TencentCaptcha").click(),onIsOff=!1))}),1e3);
