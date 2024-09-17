d6b7e226d42903f001d7cc3d0e0b00e50b321088
MUG-chen <hanming.chen@outlook.com>
2024-09-17 16:47:39 +0800

    Site updated: 2024-09-17 16:47:33

--- libs/codeBlock/codeBlockFuction.js
+++ libs/codeBlock/codeBlockFuction.js
@@ -1,5 +1,1 @@
-// 代码块功能依赖
-
-$(function () {
-    $('pre').wrap('<div class="code-area" style="position: relative"></div>');
-});
+$((function(){$("pre").wrap('<div class="code-area" style="position: relative"></div>')}));
